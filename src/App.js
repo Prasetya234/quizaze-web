352fc420f4e014478d09512f326310064897fdc7
Prasetya234 <putraprasetyadhany@gmail.com>
2022-11-03 19:16:04 +0700

    hide copy right

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import './style/App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/App.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { React, useState } from 'react';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import google from './assets/icon/google.jpeg';
-import react from './assets/icon/react.png';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { React, useState } from 'react'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import google from './assets/icon/google.jpeg'
+import react from './assets/icon/react.png'
 
 import Congrats from './pages/congrats/Index'
-import Home from './pages/home/Index';
-import Admin from './pages/admin/Index';
-import Question from './pages/question/Index';
-import NotFound from './pages/404/Index';
+import Home from './pages/home/Index'
+import Admin from './pages/admin/Index'
+import Question from './pages/question/Index'
+import NotFound from './pages/404/Index'
 import EditQuestion from './pages/edit-question/Index'
-import CreateQuestion from './pages/create-question/Index';
+import CreateQuestion from './pages/create-question/Index'
 
-import Modal from './components/modal/Modal';
-import Stars from './components/stars/Index';
+import Modal from './components/modal/Modal'
+import Stars from './components/stars/Index'
 
-import { play } from './util/generateMusic';
+import { play } from './util/generateMusic'
 
 axios.interceptors.response.use(undefined, async (err) => {
   if (err.response.status === 401) {
     await Swal.fire({
       text: 'Your session has expired. Please reload the page or click the button',
       confirmButtonText: 'Reload',
-      icon: 'info',
-    });
-    window.location.reload();
+      icon: 'info'
+    })
+    window.location.reload()
   } else if (err.response.status === 0) {
     await Swal.fire({
       icon: 'warning',
@@ -39,25 +39,33 @@ axios.interceptors.response.use(undefined, async (err) => {
       text: 'Aplikasi ini sedang dilakukan maintenance. \n Mohon periksa kembali nanti.',
       confirmButtonText: 'Close',
       showClass: {
-        popup: 'animate__animated animate__fadeInDown',
+        popup: 'animate__animated animate__fadeInDown'
       },
       hideClass: {
-        popup: 'animate__animated animate__fadeOutUp',
-      },
-    });
-    window.open('', '_self').close();
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+    })
+    window.open('', '_self').close()
   }
-  return Promise.reject(err);
-});
+  return Promise.reject(err)
+})
 
 function App() {
-  const [modalAbout, setModalAbout] = useState(false);
+  const [modalAbout, setModalAbout] = useState(false)
 
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <>
       <Stars />
-      <Modal title="Tentang kami" active={modalAbout} close={() => { play(); setModalAbout(false); }} height="auto">
+      <Modal
+        title="Tentang kami"
+        active={modalAbout}
+        close={() => {
+          play()
+          setModalAbout(false)
+        }}
+        height="auto"
+      >
         <AbautMe />
       </Modal>
       <div className="App">
@@ -74,13 +82,17 @@ function App() {
             </Routes>
           </BrowserRouter>
         </header>
-        <div className="copyright" onClick={() => { play(); setModalAbout(true); }} id="content">
+        {/* <div className="copyright" onClick={() => { play(); setModalAbout(true); }} id="content">
+          &copy;Copyright
+          {new Date().getFullYear()}
+        </div> */}
+        <div className="copyright">
           &copy;Copyright
           {new Date().getFullYear()}
         </div>
       </div>
     </>
-  );
+  )
 }
 
 function AbautMe() {
@@ -99,12 +111,10 @@ function AbautMe() {
       <p>Quizaze adalah website yang menyediakan soal dengan tampilan yang menarik untuk kamu yang cepat bosan dalam mengerjakan tugas-tugas sekolah</p>
       <p style={{ fontWeight: 'bold', marginBottom: '5px', fontSize: '20px' }}>Developer</p>
       <p>
-        <span onClick={() => window.open('https://www.instagram.com/xzy_prasetya/')}>Prasetya Dhany Putra</span>
-        {' '}
-        <b>(Leader & developer quizaze)</b>
+        <span onClick={() => window.open('https://www.instagram.com/xzy_prasetya/')}>Prasetya Dhany Putra</span> <b>(Leader & developer quizaze)</b>
       </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
